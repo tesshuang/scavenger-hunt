87c8418aa36cbbf7293c86e033615738fd43f97a
Tess Huang <tesshuangxj@yahoo.com>
2021-08-30 22:06:27 -0700

    Add Layout comp

---
+++ src/components/Layout/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Layout.module.css';
+
+const Layout = ({ children }) => {
+
+  return (
+    <main className={styles.container}>{children}</main>
+  )
+}
+
+export default Layout;
