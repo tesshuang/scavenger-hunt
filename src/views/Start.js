90a4b88461c9eb170f55f05c66edd8d8527ca90b
Tess Huang <tesshuangxj@yahoo.com>
2021-08-30 22:06:50 -0700

    Add Start view

---
+++ src/views/Start.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Heading from '../components/Heading';
+import Button from '../components/Button';
+
+const Start = () => (
+  <div>
+    <Heading title="Virtual Scavenger Hunt" />
+    <Button label="Start Game" />
+  </div>
+  
+)
+
+export default Start;
