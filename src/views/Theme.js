3a2ec3a6de7d787259213e2c24993cd89432acf3
Tess Huang <tesshuangxj@yahoo.com>
2021-08-31 20:54:40 -0700

    Add Select to Theme view

--- src/views/Theme.js
+++ src/views/Theme.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Heading from '../components/Heading';
+import Select from '../components/Select';
 import Button from '../components/Button';
 
 const Theme = ({ handleView }) => (
   <div>
     <Heading title="Hunting Theme" />
+    <Select />
     <Button label="Next" onClick={() => handleView('players')}/>
   </div>
   
