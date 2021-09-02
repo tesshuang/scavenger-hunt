3702b644a9489088605115592a3f4ecdfb833d09
Tess Huang <tesshuangxj@yahoo.com>
2021-09-01 21:20:43 -0700

    Add Player view

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import Layout from './components/Layout';
 import Start from './views/Start';
 import Theme from './views/Theme';
+import Players from './views/Players';
 
 function App() {
   const [currentView, setCurrentView] = useState('start');
   const [gameTheme, setGameTheme] = useState('indoor');
-  console.log(gameTheme);
+  const [players, setPlayers] = useState([]);
+
+  console.log(players);
   return (
     <Layout>
       {currentView === 'start' && <Start handleView={setCurrentView} />}
       {currentView === 'theme' && <Theme handleView={setCurrentView} handleTheme={setGameTheme}/>}
+      {currentView === 'players' && <Players handleView={setCurrentView} players={players} handlePlayers={setPlayers} />}
     </Layout>
     
   );
