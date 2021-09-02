05d79d55db8171831b4ee1ff8577f2ba6f115f3e
Tess Huang <tesshuangxj@yahoo.com>
2021-09-01 21:05:30 -0700

    Update Input with related props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, onChange }) => {
+const Input = ({ label, onChange, onKeyPress, initialValue }) => {
   
   return (
     <div className={styles.container}>
       <label>{label}</label>
-      <input className={styles.input} onChange={onChange} />
+      <input className={styles.input} value={initialValue} onChange={onChange} onKeyPress={onKeyPress}/>
     </div>
     
   )
