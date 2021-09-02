fea65df6c2d6527367d6594a4ac0eccc36150506
Tess Huang <tesshuangxj@yahoo.com>
2021-09-01 21:07:44 -0700

    Add addPlayer funcs and display players

--- src/views/Players.js
+++ src/views/Players.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Heading from '../components/Heading';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
-const Players = ({ handleView }) => (
-  <div>
-    <Heading title="Players" />
-    <Input label="Enter some players" />
-    <div></div>
-    <Button label="Player Game" onClick={() => handleView('theme')} />
-  </div>
-  
-)
+const Players = ({ handleView, players, handlePlayers }) => {
+  const [name, setName] = useState('');
+
+  const addPlayers = (e) => {
+    if(e.key === 'Enter' && name !== '') {
+      console.log('enter a new player');
+      handlePlayers([...players, name]);
+      setName('');
+    }
+  }
+
+  return (
+    <div>
+      <Heading title="Players" />
+      <Input label="Enter some players" onChange={(e) => setName(e.target.value)} onKeyPress={addPlayers} initialValue={name}/>
+      {players.length > 0 && (
+        <div>
+          {players.map((player, index) => (
+            <div key={index}>{player}</div>
+          ))}
+        </div>
+      )}
+      <Button label="Player Game" onClick={() => handleView('theme')} />
+    </div>
+)}
 
 export default Players;
